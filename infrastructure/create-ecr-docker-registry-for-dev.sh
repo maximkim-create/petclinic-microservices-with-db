ad9d668e9e12d3066429de1c04d0bdffe8f3271a
maximkim-create <usmaxim@gmail.com>
2021-08-07 14:58:56 +0000

    added scripts for qa automation environment

--- infrastructure/create-ecr-docker-registry-for-dev.sh
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -1,7 +1,7 @@
 PATH="$PATH:/usr/local/bin"
-APP_REPO_NAME="maximkim2/petclinic-app-dev"
+APP_REPO_NAME="maxim-repo/petclinic-app-dev"
 AWS_REGION="us-east-1"
-echo 'Creating ECRrepo'
+
 aws ecr create-repository \
   --repository-name ${APP_REPO_NAME} \
   --image-scanning-configuration scanOnPush=false \
